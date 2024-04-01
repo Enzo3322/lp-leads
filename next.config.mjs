6724f91b19bda6426e4dec759e5acb86b3e64ac3
Enzo3322 <enzo.spag14@gmail.com>
2024-04-01 09:41:23 -0300

    update action

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   reactStrictMode: true,
   output: "export",
+  basePath: "/lp-leads",
 };
 
 export default nextConfig;
