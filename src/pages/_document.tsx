05628600bb289c791bb99496023592ee1d113c85
Enzo3322 <enzo.spag14@gmail.com>
2024-10-22 09:01:48 -0300

    add doc title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head>
+        <title>Landing page template</title>
         <meta name="title" content="Landing page template" />
         <meta name="description" content="Landing page template" />
       </Head>
